ecee90c8169ac7a991a2c9a62a359405cc64882a
dhimanaditi <100407898+dhimanaditi@users.noreply.github.com>
2023-11-19 19:48:40 +0530

    Update Two-pointer-technique.java

--- Two-pointer-technique.java
+++ Two-pointer-technique.java
@@ -271,3 +271,29 @@ public class Main{
     }
     
 }
+
+
+//Here is an example of how to use the two pointer technique when the value is of type string in Java:
+public class TwoPointerTechnique {
+
+    public static void main(String[] args) {
+        String str = "Hello, World!";
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) == str.charAt(right)) {
+                left++;
+                right--;
+            } else {
+                break;
+            }
+        }
+
+        if (left == right) {
+            System.out.println("The string is a palindrome.");
+        } else {
+            System.out.println("The string is not a palindrome.");
+        }
+    }
+}
